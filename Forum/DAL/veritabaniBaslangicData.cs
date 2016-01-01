0cee5f437d0d73f24731646db89307505f52f89e
Burak Demirci <burakdemirci.34@gmail.com>
2016-01-02 00:17:26 +0200

    DAL ve web config de veritabanı ayarları yapıldı.

---
+++ Forum/DAL/veritabaniBaslangicData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Forum.Models;
+
+namespace Forum.DAL
+{
+    public class veritabaniBaslangicData:System.Data.Entity.DropCreateDatabaseIfModelChanges<veritabaniContext>
+    {
+        protected override void Seed(veritabaniContext context)
+        {
+            var Kullanicilar = new List<Kullanicilar>
+            {
+                new Kullanicilar {KullanicilarID=1,Ad="Burak",Soyad="Demirci",Email="burakdemirci.34@gmail.com",KullaniciAdi="kurkix" }
+            };
+
+            Kullanicilar.ForEach(s => context.KullaniciTablosu.Add(s));
+            context.SaveChanges();
+            var Kategoriler = new List<Kategoriler>
+            {
+                new Kategoriler {KategorilerID=1,KategoriAdi="Deneme",KategoriAciklama="Bu kategori deneme amaçlı açılmıştır." }
+            };
+            Kategoriler.ForEach(s => context.KategoriTablosu.Add(s));
+            context.SaveChanges();
+            var Konular = new List<Konular>
+            {
+                new Konular {KonularID=1,Baslik="Hoşgeldiniz",Icerik="Forum sistemine hoşgeldiniz." }
+            };
+            Konular.ForEach(s => context.KonuTablosu.Add(s));
+            context.SaveChanges();
+
+        }
+    }
+}
---
+++ Forum/DAL/veritabaniContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Forum.Models;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Forum.DAL
+{
+    public class veritabaniContext:DbContext
+    {
+        public veritabaniContext():base ("veritabaniContext")
+        {
+
+        }
+        public DbSet<Kategoriler> KategoriTablosu { get; set; }
+        public DbSet<Konular> KonuTablosu { get; set; }
+        public DbSet<Kullanicilar> KullaniciTablosu { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+    
+}
